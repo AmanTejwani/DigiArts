16a74e10e8492df79e6701a3e06f5cadf8fb233c
AmanTejwani <amantejwani17@gmail.com>
2023-04-01 00:14:03 +0530

    Form Submit added in modal and connected with global state

--- src/components/CreateModal.jsx
+++ src/components/CreateModal.jsx
@@ -16,15 +16,25 @@ const CreateModal = () => {
         resetForm()
      }
 
-     const handleTitleChange=(e)=>{
-        setTitle(e.target.value)
+     const handleTitleChange=(event)=>{
+        setTitle(event.target.value)
      }
-     const handlePriceChange=(e)=>{
-        setPrice(e.target.value)
+     const handlePriceChange=(event)=>{
+        setPrice(event.target.value)
      }
-     const handleDescriptionChange=(e)=>{
-        setDescription(e.target.value)
+     const handleDescriptionChange=(event)=>{
+        setDescription(event.target.value)
      }
+     const handleSubmit=(event)=>{
+      event.preventDefault()
+      setGlobalState('modal', 'scale-0')
+      if(!title || !description || !price)
+        return;
+      console.log("minitng your nft")
+
+      resetForm()
+     }
+
      const resetForm = () => {
         setTitle('')
         setPrice('')
@@ -32,6 +42,7 @@ const CreateModal = () => {
         setUrl('')
         setImgBase64(null)
     }
+
 
   return (
     <div className={`fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-50 transform transition-transform duration-300 ${modal}`}>
@@ -45,12 +56,12 @@ const CreateModal = () => {
             </button>
           </div>
 
-          <div className="flex flex-row justify-center items-center rounded-xl mt-5">
+          {/* <div className="flex flex-row justify-center items-center rounded-xl mt-5">
             <div className="shrink-0 rounded-xl overflow-hidden h-20 w-20">
-                <img alt="NFT"  className="h-full w-full object-cover cursor-pointer" src='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80'/>
+                <img alt="NFT"  className="h-full w-full object-cover cursor-pointer" src='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80'/> */}
                 {/* <img alt="NFT"  className="h-full w-full object-cover cursor-pointer" src={ imgBase64 || 'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80'}/> */}
-            </div>
-          </div>
+            {/* </div>
+          </div> */}
 
           <div className="flex flex-row justify-between items-center bg-gray-800 rounded-xl mt-5">
             <label className="block">
@@ -73,9 +84,10 @@ const CreateModal = () => {
             <textarea className="block w-full text-sm resize-none text-slate-500 bg-transparent border-0 focus:outline-none focus:ring-0 h-20" type="text" name="description" placeholder="Description" onChange={handleDescriptionChange} value={description} required> </textarea>
           </div>
 
-          {/* <button type="submit" onClick={handleSubmit} className="flex flex-row justify-center items-center w-full text-white text-md bg-[#e32970] hover:bg-[#bd255f] py-2 px-5 rounded-full drop-shadow-xl border border-transparent hover:bg-transparent hover:text-[#e32970] hover:border hover:border-[#bd255f] focus:outline-none focus:ring mt-5" >
-            Mint Now
-          </button> */}
+          <button type="submit" onClick={handleSubmit} className="flex flex-row justify-center items-center w-full text-white text-md bg-[#e32970] hover:bg-[#bd255f] py-2 px-5 rounded-full drop-shadow-xl border border-transparent hover:bg-transparent hover:text-[#e32970] hover:border hover:border-[#bd255f] focus:outline-none focus:ring mt-5" >
+            Mint NFT
+          </button>
+          
         </form>
       </div>
     </div>
