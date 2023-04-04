9e71d13ec63f220f60f4e2814a3f00a22b8e1d98
NisargShah <2121nisarg@gmail.com>
2023-04-04 06:31:11 +0530

    Done 90%

--- src/components/ArtCollection.jsx
+++ src/components/ArtCollection.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { setGlobalState , useGlobalState , getGlobalState } from '../store'
-// import imageHero from 'https://images.cointelegraph.com/images/1434_aHR0cHM6Ly9zMy5jb2ludGVsZWdyYXBoLmNvbS91cGxvYWRzLzIwMjEtMDYvNGE4NmNmOWQtODM2Mi00YmVhLThiMzctZDEyODAxNjUxZTE1LmpwZWc=.jpg'
+import { useEffect, useState } from 'react'
+import { setGlobalState , useGlobalState } from '../store'
+
 const ArtCollection = () => {
   const [nfts] = useGlobalState('nfts')
   const [end, setEnd] = useState(4)
@@ -19,7 +20,6 @@ const ArtCollection = () => {
         <div className="w-4/5 py-10 mx-auto">
             <h4 className="text-white text-3xl font-bold uppercase text-gradient">
             {collection.length > 0 ? 'Latest Artworks' : 'No Artworks Yet'}
-                {/* Latest Artworks */}
             </h4>      
 
             <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-4 lg:gap-3 py-2.5">
--- src/components/CreateModal.jsx
+++ src/components/CreateModal.jsx
@@ -8,79 +8,96 @@ import {
 } from '../store'
 import { mintNFT } from '../Blockchain.Services'
 import { useState } from 'react'
+import { create } from 'ipfs-http-client'
+
+const auth =
+  'Basic ' +
+  Buffer.from(
+    'e414935d16695435beb5' + ':' + '03fb82baa6e3080a57952c55e86d9d9021f16a5938119d60ec6ce6684e5e5b4a',
+  ).toString('base64')
+
+const client = create({
+  host: 'ipfs.infura.io',
+  port: 5001,
+  protocol: 'https',
+  headers: {
+    authorization: auth,
+  },
+})
 
 const CreateModal = () => {
      const [modal] =useGlobalState('modal')
      const [title,setTitle] = useState('')
      const [price,setPrice] = useState('')
      const [description,setDescription] = useState('')
-     const [url,setUrl] = useState('')
+     const [fileUrl, setFileUrl] = useState('')
      const [imgBase64,setImgBase64] = useState(null)
 
-     const changeImage = async (e) => {
+     
+     
+     const handleTitleChange=(event)=>{
+       setTitle(event.target.value)
+      }
+     const handlePriceChange=(event)=>{
+        setPrice(event.target.value)
+     }
+     const handleDescriptionChange=(event)=>{
+       setDescription(event.target.value)
+      }
+    
+      const handleSubmit = async (e) => {
+      e.preventDefault()
+  
+      if (!title || !price || !description) return
+      
+      setGlobalState('modal', 'scale-0')
+      setGlobalState('loading', { show: true, msg: 'Uploading IPFS data...' })
+      
+      try {
+        const created = await client.add(fileUrl)
+        const metadataURI = `https://ipfs.io/ipfs/${created.path}`
+        const nft = { title, price, description, metadataURI }
+        
+        setLoadingMsg('Intializing transaction...')
+        setFileUrl(metadataURI)
+        await mintNFT(nft)
+        
+        resetForm()
+        setAlert('Minting completed...', 'green')
+        window.location.reload()
+      } catch (error) {
+        console.log('Error uploading file: ', error)
+        setAlert('Minting failed...', 'red')
+      }
+    }
+    
+    const changeImage = async (e) => {
       const reader = new FileReader()
       if (e.target.files[0]) reader.readAsDataURL(e.target.files[0])
-  
+      
       reader.onload = (readerEvent) => {
         const file = readerEvent.target.result
         setImgBase64(file)
         setFileUrl(e.target.files[0])
       }
     }
-  
+
     const closeModal = () => {
       setGlobalState('modal', 'scale-0')
       resetForm()
-    }
-
-     const handleTitleChange=(event)=>{
-        setTitle(event.target.value)
      }
-     const handlePriceChange=(event)=>{
-        setPrice(event.target.value)
-     }
-     const handleDescriptionChange=(event)=>{
-        setDescription(event.target.value)
-     }
-    
-  const handleSubmit = async (e) => {
-    e.preventDefault()
 
-    if (!title || !price || !description) return
-
-    setGlobalState('modal', 'scale-0')
-    setGlobalState('loading', { show: true, msg: 'Uploading IPFS data...' })
-
-    try {
-      const created = await client.add(fileUrl)
-      const metadataURI = `https://ipfs.io/ipfs/${created.path}`
-      const nft = { title, price, description, metadataURI }
-
-      setLoadingMsg('Intializing transaction...')
-      setFileUrl(metadataURI)
-      await mintNFT(nft)
-
-      resetForm()
-      setAlert('Minting completed...', 'green')
-      window.location.reload()
-    } catch (error) {
-      console.log('Error uploading file: ', error)
-      setAlert('Minting failed...', 'red')
-    }
-  }
-
-     const resetForm = () => {
-        setTitle('')
+    const resetForm = () => {
+       setTitle('')
         setPrice('')
         setDescription('')
-        setUrl('')
+        setFileUrl('')
         setImgBase64(null)
-    }
+      }
+      
 
-
-  return (
-    <div className={`fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-50 transform transition-transform duration-300 ${modal}`}>
-    {/* <div className='fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-50 transform transition-transform duration-300 scale-100'> */}
+      return (
+        <div className={`fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-50 transform transition-transform duration-300 ${modal}`}>
       <div className="bg-[#151c25] shadow-xl shadow-[#00cc66] rounded-xl w-11/12 md:w-2/5 h-7/12 p-6">
         <form className="flex flex-col">
           <div className="flex flex-row justify-between items-center">
@@ -90,12 +107,11 @@ const CreateModal = () => {
             </button>
           </div>
 
-          {/* <div className="flex flex-row justify-center items-center rounded-xl mt-5">
+          <div className="flex flex-row justify-center items-center rounded-xl mt-5">
             <div className="shrink-0 rounded-xl overflow-hidden h-20 w-20">
-                <img alt="NFT"  className="h-full w-full object-cover cursor-pointer" src='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80'/> */}
-                {/* <img alt="NFT"  className="h-full w-full object-cover cursor-pointer" src={ imgBase64 || 'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80'}/> */}
-            {/* </div>
-          </div> */}
+                <img alt="NFT"  className="h-full w-full object-cover cursor-pointer" src={ imgBase64 || 'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80'}/>
+            </div>
+          </div>
 
           <div className="flex flex-row justify-between items-center bg-gray-800 rounded-xl mt-5">
             <label className="block">
